c49ee7e389f0c61959a80cfdec09c30a0c2a5658
Fabricio Jardin dos Santos <fabricio.jardin@moovin.com.br>
2023-04-27 12:56:37 -0300

    Remove código padrão do t3-app da home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Unite to help</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
