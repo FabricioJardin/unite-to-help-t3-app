6dd3cd916e8b022ceeb9c46e98f5cd422aabe30d
Fabricio Jardin dos Santos <fabriciojardin.dev@gmail.com>
2023-04-28 18:25:52 -0300

    Altera carregamento de envs com a nova lib t3-oss/auth-nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,14 +21,15 @@ const AuthShowcase: React.FC = () => {
   );
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className="flex flex-col items-center justify-center gap-4 ">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
+      <img src={sessionData?.user.image || ''} />
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
+        onClick={sessionData ? () => void signOut() : () => void signIn('google')}
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,10 +1,7 @@
 import { type GetServerSidePropsContext } from "next";
-import {
-  getServerSession,
-  type NextAuthOptions,
-  type DefaultSession,
-} from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import { getServerSession, type NextAuthOptions, type DefaultSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -44,12 +41,49 @@ export const authOptions: NextAuthOptions = {
         id: user.id,
       },
     }),
+    // async signIn({ account, user, credentials, profile, email }) {
+
+    //   console.log({ account, user, credentials, profile, email })
+
+    //   return false
+    // }
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "E-mail e senha",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      // credentials: {
+      //   email: { label: "E-mail", type: "email", placeholder: "user@example.com" },
+      //   password: { label: "Senha", type: "password", placeholder: '**********' }
+      // },
+      authorize(credentials, req) {
+        console.log({ credentials, req });
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: credentials?.email };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
     }),
     /**
      * ...add more providers here.
