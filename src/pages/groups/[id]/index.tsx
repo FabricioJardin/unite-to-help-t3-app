013e96cc0ad84384b594770a8fd147ecedc484c2
Fabricio Jardin dos Santos <fabriciojardin.dev@gmail.com>
2023-05-13 22:04:45 -0300

    feat: Adiciona botão e role do usuário admin para entrar em contato

--- src/pages/groups/[id]/index.tsx
+++ src/pages/groups/[id]/index.tsx
@@ -1,9 +1,11 @@
+import { UserIcon } from "lucide-react"
 import { MapPinIcon } from "lucide-react"
 import { type GetServerSidePropsContext } from "next"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import MainLayout from "~/components/main-layout"
 import { useToast } from "~/hooks/use-toast"
+import { Avatar, AvatarFallback, AvatarImage } from "~/ui/avatar"
 import { Badge } from "~/ui/badge"
 import { Button } from "~/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/ui/card"
@@ -39,21 +41,28 @@ function GroupDetailPage({ addressData }: GroupDetailPageProps) {
     },
   })
 
+  const handleAdminContact = () => {
+    toast({
+      title: "Funcionalidade indisponível...",
+      description: "A funcionalidade ainda está em desenvolvimento.",
+    })
+  }
+
   return (
     <MainLayout>
       <div className="flex h-screen items-center justify-center">
         <Card className="min-w-full rounded-3xl md:min-w-[925px] md:max-w-[925px]">
           <CardHeader>
-            <CardTitle>{group?.name}</CardTitle>
+            <CardTitle className="text-3xl">{group?.name}</CardTitle>
             <CardDescription>{group?.description}</CardDescription>
           </CardHeader>
           <CardContent>
             <div className="flex flex-col gap-5">
-              <div className="flex gap-2">
+              <div className="flex items-center gap-2">
                 <MapPinIcon />
 
                 {addressData ? (
-                  <span>
+                  <span className="text-2xl">
                     {addressData.neighborhood} - {addressData.city} {addressData.state}
                   </span>
                 ) : (
@@ -62,7 +71,8 @@ function GroupDetailPage({ addressData }: GroupDetailPageProps) {
               </div>
 
               <div className="flex flex-col gap-2">
-                <Label>Contatos</Label>
+                <h1 className="text-2xl">Contatos</h1>
+                {!group?.contacts.length && <p>O grupo não possui contatos disponíveis.</p>}
                 {group?.contacts.map((contact) => (
                   <div className="flex gap-2" key={contact.id}>
                     <span>{contact.type === "EMAIL" ? "Email" : "Telefone"}: </span>
@@ -72,12 +82,44 @@ function GroupDetailPage({ addressData }: GroupDetailPageProps) {
               </div>
 
               <div className="flex flex-col gap-2">
-                <Label>Causas</Label>
+                <h1 className="text-2xl">Causas</h1>
                 <div className="flex flex-wrap gap-2">
                   {group?.causes.map((cause) => (
                     <Badge key={cause.id} className="gap-1 px-1">
                       <span>{cause.name}</span>
                     </Badge>
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-3">
+                <h1 className="text-2xl">Usuários</h1>
+                <div className="flex flex-col gap-1">
+                  {group?.users.map((userOnGroup) => (
+                    <div className="flex  rounded-md border bg-muted p-2" key={userOnGroup.userId}>
+                      <div className="flex flex-1 items-center space-x-4">
+                        <Avatar>
+                          <AvatarImage src={userOnGroup.user.image || undefined} />
+                          <AvatarFallback className="text-white">
+                            {userOnGroup.user.name
+                              ?.split(" ")
+                              .map((v) => v.at(0)?.toUpperCase())
+                              .join("") || <UserIcon />}
+                          </AvatarFallback>
+                        </Avatar>
+
+                        <p className="text-sm font-medium leading-none">{userOnGroup.user.name}</p>
+                      </div>
+
+                      {userOnGroup.role === "ADMIN" && (
+                        <div className="flex w-full flex-1 items-center justify-end gap-2">
+                          <Badge>Admin</Badge>
+                          <Button className="rounded-lg" onClick={handleAdminContact}>
+                            Entrar em contato
+                          </Button>
+                        </div>
+                      )}
+                    </div>
                   ))}
                 </div>
               </div>
